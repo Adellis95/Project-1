cc39216408086e0dedc083e6db88f00966900888
Dempsey FP <dempseyfinley@gmail.com>
2020-10-14 20:38:50 -0400

    api call for characters in location/3

--- script.js
+++ script.js
@@ -2,5 +2,23 @@ console.log("hello project 1");
 
 $("#startAdv").click(function(){
     console.log("You clicked me!")
+        let queryURL = "https://rickandmortyapi.com/api/location/3";
+        $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).then(function(res) {
+      console.log(res.residents);
+      for(i=0;i<res.residents.length;i++){
+          let names = res.residents;
+          let citadelNames = $(`
+          <div class="card">
+          <p>${names[i]}</p>
+          </div>
+          `)
+    $("#test").append(citadelNames);
+
+          
+    }
+})
 });
 

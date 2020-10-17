0dbf12892fa0949faa712af9f4934815a937e316
chyppoli <claude.hyppolite@gmail.com>
2020-10-17 10:30:59 -0400

    reset the script.js file

--- script.js
+++ script.js
@@ -1,92 +1,109 @@
 $(document).ready(function () {
-  // set initial background image
   var imageUrl =
     "https://i.pinimg.com/originals/e2/97/8c/e2978ca3d3a608d8f3e3dac5c083f3cb.jpg";
-
-  setBgImg(imageUrl);
-
-  // citadel of the ricks background
-  // var citadelBgImg =
-  //   "https://vignette.wikia.nocookie.net/rickandmorty/images/8/85/Rick-and-morty-citadel-of-rick-750x411.jpg/revision/latest?cb=20160903134247";
-
   var citadelBgImg =
     "https://cdn-images-1.medium.com/max/1280/1*BArwiczvwUqxWu9OB1e7Sw.png";
 
-  // world 3 = citadel and world 4 = world ender
-  var worldArray = [3, 4];
+  setBgImg(imageUrl);
 
-  console.log("hello project 1");
+  // hides next world navigation button
+  $("#btn-next-world").hide();
 
-  // start adventure button listener
-  $("#startAdv").click(function () {
-    console.log("You clicked me!");
-    let cardDisplay= $(`
-    <div class="container">
-    <div class="row">
-    <div class="card col-md-6" style="width: 18rem;">
-  <img src="https://rickandmortyapi.com/api/character/avatar/274.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Quantum Rick</p>
-  </div>
-</div>
-<div class="card col-md-6" style="width: 18rem;">
-  <img src="https://rickandmortyapi.com/api/character/avatar/48.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Black Rick</p>
-  </div>
-</div>
-</div>
-<div class="row">
-<div class="card col-md-6" style="width: 18rem;">
-  <img src="https://rickandmortyapi.com/api/character/avatar/267.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Plumber Rick</p>
-  </div>
-</div>
-<div class="card col-md-6" style="width: 18rem;">
-  <img src="https://rickandmortyapi.com/api/character/avatar/119.jpeg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Evil Rick</p>
-  </div>
-</div>
-</div>
-</div>
-`)
-$("#test").append(cardDisplay);
-    // let queryURL = "https://rickandmortyapi.com/api/location/3";
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET",
-    // }).then(function (res) {
-    //   for (i = 0; i < res.residents.length; i++) {
-    //     let names = res.residents[i];
-    //     $.ajax({
-    //       url: names,
-    //       method: "GET",
-    //     }).then(function (res) {
-    //       let nameList = res.name.slice(0, res.name.length);
-
-    //       console.log(nameList);
-    //       let citadelNames = $(`
-    //       <div class="card">
-    //       <p>${nameList[Math.floor(Math.random() * nameList.length)]}</p>
-    //       </div>
-    //       `);
-    //       $("#test").append(citadelNames);
-    //     });
-    //   }
-    // });
-
-    // set the first world
+  $("#start-adventure").click(function () {
     setBgImg(citadelBgImg);
+    clearHomePageText();
+    addCards();
+    addCards1();
+    addCards3();
+
+    // default thumbnail sizing
+    $(".img-thumbnail").css("width", "100%");
+    $(".img-thumbnail").css("height", "100%");
+
+    $("#start-adventure").hide();
+    $("#btn-next-world").show();
   });
 
+  //   fixed footer styling
+
+  $(".footer").css("position", "fixed");
+  $(".footer").css("left", "0");
+  $(".footer").css("bottom", "0");
+  $(".footer").css("width", "100%");
+  $(".footer").css("height", "10vh");
+
   function setBgImg(URL) {
-    $("body").css("background-image", "url(" + URL + ")");
-    $("body").css("background-size", "cover");
-    $("body").css("background-attachment", "fixed");
-    $("content").css("width", "50%");
-    $("content").css("padding", "40px");
-    $("content").css("margin", "100px auto");
+    $(".jumbotron").css("background-image", "url(" + URL + ")");
+    $(".jumbotron").css("background-size", "100% 50%");
+    // $(".jumbotron").css("background-position", "center center");
+    $(".jumbotron").css("background-attachment", "fixed");
+    // $(".jumbotron").css("background-repeat", "no");
+    $(".jumbotron").css("height", "50vh");
+    // $("content").css("width", "50%");
+    // $("content").css("padding", "40px");
+    // $("content").css("margin", "100px auto");
+  }
+
+  function clearHomePageText() {
+    $("#jumbotron-text").empty();
+    $("#blockquote-main").empty();
+    $("#blockquote-cite-main").empty();
+    $("#blockquote-cite-main").removeClass("blockquote-footer");
+    $("#game-instructions").empty();
+  }
+
+  function addCards() {
+    var cardImgEl = $("<img>");
+
+    cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
+    cardImgEl.attr(
+      "src",
+      "https://rickandmortyapi.com/api/character/avatar/274.jpeg"
+    );
+    // cardImgEl.attr("width", "100px");
+    // cardImgEl.attr("height", "150px");
+    $("#character-card-1").addClass("card bg-light");
+    $("#character-card-1").attr("style", "height: 15rem;");
+    $("#character-card-1").append(cardImgEl);
+    $("#character-card-1").append(
+      "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
+    );
+  }
+
+  //   https://rickandmortyapi.com/api/character/avatar/119.jpeg
+  function addCards1() {
+    var cardImgEl = $("<img>");
+
+    cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
+    cardImgEl.attr(
+      "src",
+      "https://rickandmortyapi.com/api/character/avatar/267.jpeg"
+    );
+    // cardImgEl.attr("width", "100px");
+    // cardImgEl.attr("height", "150px");
+    $("#character-card-2").addClass("card");
+    $("#character-card-2").attr("style", "height: 15rem;");
+    $("#character-card-2").append(cardImgEl);
+    $("#character-card-2").append(
+      "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
+    );
+  }
+
+  function addCards3() {
+    var cardImgEl = $("<img>");
+
+    cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
+    cardImgEl.attr(
+      "src",
+      "https://rickandmortyapi.com/api/character/avatar/119.jpeg"
+    );
+    // cardImgEl.attr("width", "5rem");
+    // cardImgEl.attr("height", "10px");
+    $("#character-card-3").addClass("card");
+    $("#character-card-3").attr("style", "height: 15rem;");
+    $("#character-card-3").append(cardImgEl);
+    $("#character-card-3").append(
+      "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
+    );
   }
 });

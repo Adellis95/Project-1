4c382d7e72c1723e55af02d9876cd04c1eda4abd
chyppoli <claude.hyppolite@gmail.com>
2020-10-17 13:27:16 -0400

    add card flip animation

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
+  var imageUrl =
+    "https://i.pinimg.com/originals/e2/97/8c/e2978ca3d3a608d8f3e3dac5c083f3cb.jpg";
+  var citadelBgImg =
+    "https://cdn-images-1.medium.com/max/1280/1*BArwiczvwUqxWu9OB1e7Sw.png";
 
-    var imageUrl =
-      "https://i.pinimg.com/originals/e2/97/8c/e2978ca3d3a608d8f3e3dac5c083f3cb.jpg";
-    var citadelBgImg =
-      "https://cdn-images-1.medium.com/max/1280/1*BArwiczvwUqxWu9OB1e7Sw.png";
-  
-    setBgImg(imageUrl);
-  
-    // hides next world navigation button
-    $("#btn-next-world").hide();
-  
-    $("#start-adventure").click(function () {
-      setBgImg(citadelBgImg);
-      clearHomePageText();
-      addCards();
-      addCards1();
-      addCards3();
-  
-      // default thumbnail sizing
-      $(".img-thumbnail").css("width", "100%");
-      $(".img-thumbnail").css("height", "100%");
-  
-      $("#start-adventure").hide();
-      $("#btn-next-world").show();
-    });
-  
+  setBgImg(imageUrl);
+
+  // hides next world navigation button
+  $("#btn-next-world").hide();
+
+  $("#start-adventure").click(function () {
+    setBgImg(citadelBgImg);
+    clearHomePageText();
+    addCards();
+    addCards1();
+    addCards3();
+    addCards4();
+
+    // default thumbnail sizing
+    $(".img-thumbnail").css("width", "100%");
+    $(".img-thumbnail").css("height", "100%");
+
+    $("#start-adventure").hide();
+    $("#btn-next-world").show();
+  });
+
   //   fixed footer styling
 
   $(".footer").css("position", "fixed");
@@ -63,10 +63,13 @@ $(document).ready(function () {
     );
     // cardImgEl.attr("width", "100px");
     // cardImgEl.attr("height", "150px");
-    $("#character-card-1").addClass("card bg-light");
-    $("#character-card-1").attr("style", "height: 15rem;");
-    $("#character-card-1").append(cardImgEl);
-    $("#character-card-1").append(
+    $("#card-front-1").addClass("card");
+    $("#card-back-1").addClass("card");
+    $("#card-front-1").attr("style", "height: 15rem;");
+    $("#card-back-1").attr("style", "height: 15rem;");
+    $("#card-back-1").text("Bad Advice");
+    $("#card-front-1").append(cardImgEl);
+    $("#card-front-1").append(
       "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
     );
   }
@@ -82,10 +85,13 @@ $(document).ready(function () {
     );
     // cardImgEl.attr("width", "100px");
     // cardImgEl.attr("height", "150px");
-    $("#character-card-2").addClass("card");
-    $("#character-card-2").attr("style", "height: 15rem;");
-    $("#character-card-2").append(cardImgEl);
-    $("#character-card-2").append(
+    $("#card-front-2").addClass("card");
+    $("#card-back-2").addClass("card");
+    $("#card-front-2").attr("style", "height: 15rem;");
+    $("#card-back-2").attr("style", "height: 15rem;");
+    $("#card-back-2").text("Bad Advice");
+    $("#card-front-2").append(cardImgEl);
+    $("#card-front-2").append(
       "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
     );
   }
@@ -100,56 +106,74 @@ $(document).ready(function () {
     );
     // cardImgEl.attr("width", "5rem");
     // cardImgEl.attr("height", "10px");
-    $("#character-card-3").addClass("card");
-    $("#character-card-3").attr("style", "height: 15rem;");
-    $("#character-card-3").append(cardImgEl);
-    $("#character-card-3").append(
+    $("#card-front-3").addClass("card");
+    $("#card-back-3").addClass("card");
+    $("#card-front-3").attr("style", "height: 15rem;");
+    $("#card-back-3").attr("style", "height: 15rem;");
+    $("#card-back-3").text("Bad Advice");
+    $("#card-front-3").append(cardImgEl);
+    $("#card-front-3").append(
+      "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
+    );
+  }
+
+  function addCards4() {
+    var cardImgEl = $("<img>");
+
+    cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
+    cardImgEl.attr(
+      "src",
+      "https://rickandmortyapi.com/api/character/avatar/119.jpeg"
+    );
+    // cardImgEl.attr("width", "5rem");
+    // cardImgEl.attr("height", "10px");
+    $("#card-front-4").addClass("card");
+    $("#card-back-4").addClass("card");
+    $("#card-front-4").attr("style", "height: 15rem;");
+    $("#card-back-4").attr("style", "height: 15rem;");
+    $("#card-back-4").text("Bad Advice");
+    $("#card-front-4").append(cardImgEl);
+    $("#card-front-4").append(
       "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
     );
   }
 });
 
 // Ajax calls
-$("#character-card-1").click(function(){
+$("#character-card-1").click(function () {
   console.log("card 1 clicked");
   $.ajax({
     url: "https://rickandmortyapi.com/api/location",
     method: "GET",
-  }).then(function(response) {
-   console.log(response);
-   for(i = 0; i < response.data.length; i++){
-    console.log(response.data[i].images.fixed_width.url)
-  }
-   
+  }).then(function (response) {
+    console.log(response);
+    for (i = 0; i < response.data.length; i++) {
+      console.log(response.data[i].images.fixed_width.url);
+    }
   });
-
 
   $.ajax({
     url: "https://cors-anywhere.herokuapp.com/https://badadvice.rest/api",
     method: "GET",
-  }).then(function(response) {
-   console.log(response);
-   
+  }).then(function (response) {
+    console.log(response);
   });
 });
-$("#character-card-2").click(function(){
+$("#character-card-2").click(function () {
   console.log("card 2 clicked");
   $.ajax({
     url: "https://cors-anywhere.herokuapp.com/https://badadvice.rest/api",
     method: "GET",
-  }).then(function(response) {
-   console.log(response);
-   
+  }).then(function (response) {
+    console.log(response);
   });
 });
-$("#character-card-3").click(function(){
+$("#character-card-3").click(function () {
   console.log("card 3 clicked");
   $.ajax({
     url: "https://cors-anywhere.herokuapp.com/https://badadvice.rest/api",
     method: "GET",
-  }).then(function(response) {
-   console.log(response);
-   
+  }).then(function (response) {
+    console.log(response);
   });
 });
-

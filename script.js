a9c028e23bae32306819cc37e112943103a46cf4
Tornubari <t.koboo12@gmail.com>
2020-10-19 22:23:04 -0400

    added readMe

--- script.js
+++ script.js
@@ -1,112 +1,148 @@
-$(document).ready(function () {
 
-    var imageUrl =
-      "https://i.pinimg.com/originals/e2/97/8c/e2978ca3d3a608d8f3e3dac5c083f3cb.jpg";
-    var citadelBgImg =
-      "https://cdn-images-1.medium.com/max/1280/1*BArwiczvwUqxWu9OB1e7Sw.png";
-  
-    setBgImg(imageUrl);
-  
-    // hides next world navigation button
-    $("#btn-next-world").hide();
-  
-    $("#start-adventure").click(function () {
-      setBgImg(citadelBgImg);
-      clearHomePageText();
-      addCards();
-      addCards1();
-      addCards3();
-  
-      // default thumbnail sizing
-      $(".img-thumbnail").css("width", "100%");
-      $(".img-thumbnail").css("height", "100%");
-  
-      $("#start-adventure").hide();
-      $("#btn-next-world").show();
-    });
-  
-    //   fixed footer styling
-  
-    $(".footer").css("position", "fixed");
-    $(".footer").css("left", "0");
-    $(".footer").css("bottom", "0");
-    $(".footer").css("width", "100%");
-    $(".footer").css("height", "10vh");
-  
-    function setBgImg(URL) {
-      $(".jumbotron").css("background-image", "url(" + URL + ")");
-      $(".jumbotron").css("background-size", "100% 50%");
-      // $(".jumbotron").css("background-position", "center center");
-      $(".jumbotron").css("background-attachment", "fixed");
-      // $(".jumbotron").css("background-repeat", "no");
-      $(".jumbotron").css("height", "50vh");
-      // $("content").css("width", "50%");
-      // $("content").css("padding", "40px");
-      // $("content").css("margin", "100px auto");
-    }
-  
-    function clearHomePageText() {
-      $("#jumbotron-text").empty();
-      $("#blockquote-main").empty();
-      $("#blockquote-cite-main").empty();
-      $("#blockquote-cite-main").removeClass("blockquote-footer");
-      $("#game-instructions").empty();
-    }
-  
-    function addCards() {
-      var cardImgEl = $("<img>");
-  
-      cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
-      cardImgEl.attr(
-        "src",
-        "https://rickandmortyapi.com/api/character/avatar/274.jpeg"
-      );
-      // cardImgEl.attr("width", "100px");
-      // cardImgEl.attr("height", "150px");
-      $("#character-card-1").addClass("card bg-light");
-      $("#character-card-1").attr("style", "height: 15rem;");
-      $("#character-card-1").append(cardImgEl);
-      $("#character-card-1").append(
-        "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
-      );
-    }
-  
-    //   https://rickandmortyapi.com/api/character/avatar/119.jpeg
-    function addCards1() {
-      var cardImgEl = $("<img>");
-  
-      cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
-      cardImgEl.attr(
-        "src",
-        "https://rickandmortyapi.com/api/character/avatar/267.jpeg"
-      );
-      // cardImgEl.attr("width", "100px");
-      // cardImgEl.attr("height", "150px");
-      $("#character-card-2").addClass("card");
-      $("#character-card-2").attr("style", "height: 15rem;");
-      $("#character-card-2").append(cardImgEl);
-      $("#character-card-2").append(
-        "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
-      );
-    }
-  
-    function addCards3() {
-      var cardImgEl = $("<img>");
-  
-      cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
-      cardImgEl.attr(
-        "src",
-        "https://rickandmortyapi.com/api/character/avatar/119.jpeg"
-      );
-      // cardImgEl.attr("width", "5rem");
-      // cardImgEl.attr("height", "10px");
-      $("#character-card-3").addClass("card");
-      $("#character-card-3").attr("style", "height: 15rem;");
-      $("#character-card-3").append(cardImgEl);
-      $("#character-card-3").append(
-        "<div class='card-block'><h5 class='card-title'>Some Rick</h5></div>"
-      );
-    }
+$(document).ready(function () {
+  // var sunnyAudio = new Audio('screaming-sun-rick-and-morty.mp3');
+  // sunnyAudio.play();
+  var clickCount = 0;
+  var imageUrl =
+    "https://i.pinimg.com/originals/e2/97/8c/e2978ca3d3a608d8f3e3dac5c083f3cb.jpg";
+  var portalGifImg =
+    "https://realtimevfx.com/uploads/default/original/2X/9/9ec795306ce4c382f785537845c3c798aba60d07.gif";
+  var citadelBgImg =
+    "https://cdn-images-1.medium.com/max/1280/1*BArwiczvwUqxWu9OB1e7Sw.png";
+  var imageArr = [
+    "https://c4.wallpaperflare.com/wallpaper/588/5/300/rick-and-morty-toilets-hd-wallpaper-thumb.jpg",
+
+    "https://static0.srcdn.com/wordpress/wp-content/uploads/2020/03/pjimage-46.jpg?q=50&fit=crop&w=740&h=370",
+
+    "https://vignette.wikia.nocookie.net/rickandmorty/images/7/74/S4e5_2019-12-29-14h10m35s619.png/revision/latest?cb=20191229202517",
+
+    "https://filmdaily.co/wp-content/uploads/2018/06/rick-and-morty-pluto-1024x475.jpg",
+  ];
+  var myManAudio = new Audio("my-man.mp3");
+  var evilMortyAudio = new Audio(
+    "rick-and-morty-soundtrack-evil-mortys-theme-qua.mp3"
+  );
+
+  setBgImg(imageUrl);
+
+  // hides next world navigation button
+  $("#btn-next-world").hide();
+
+  // hide the character cards group to avoiding ghosting on homepage
+  $("#character-cards-group").hide();
+
+  $("#start-adventure").click(function () {
+    // myManAudio.play();
+    // evilMortyAudio.play();
+    setBgImg(citadelBgImg);
+    clearHomePageText();
+    getBadAdvice();
+    getRickAndMortyChar();
+
+    // default thumbnail sizing
+    $(".img-thumbnail").css("width", "100%");
+    $(".img-thumbnail").css("height", "100%");
+
+    $("#start-adventure").hide();
+    $("#btn-next-world").show();
+    $("#character-cards-group").show();
   });
+  // changes background image, cards, refreshes site if button is clicked more than 4 times
+  $("#btn-next-world").click(function () {
+    // myManAudio.play();
+    // evilMortyAudio.play();
+    clickCount = +clickCount + 1;
+    if (clickCount > 4) {
+      location.reload();
+    }
+
+    clearCards();
+    setBgImg(imageArr[clickCount - 1]);
+    getBadAdvice();
+    getRickAndMortyChar();
+  });
+
+  function getRickAndMortyChar() {
+    let randomArr = [];
+    for (let i = 0; i < 4; i++) {
+      randomArr.push(Math.floor(Math.random() * 591));
+    }
+    $.ajax({
+      url: "https://rickandmortyapi.com/api/character/" + randomArr,
+      method: "GET",
+    }).then(function (response) {
+      for (let i = 0; i < 4; i++) {
+        var cardImgEl = $("<img>");
+        cardImgEl.addClass("card-img-top mx-auto img-thumbnail");
+        cardImgEl.attr("src", response[i].image);
+        $("#card-front-" + [i + 1]).addClass("card");
+        $("#card-back-" + [i + 1]).addClass("card");
+        $("#card-front-" + [i + 1]).attr("style", "height: 15rem;");
+        $("#card-back-" + [i + 1]).attr("style", "height: 15rem;");
+        $("#card-back-" + [i + 1]).attr(
+          "style",
+          "font-family: MuseoModerno, cursive;"
+        );
+        $("#card-front-" + [i + 1]).append(cardImgEl);
+        $("#card-front-" + [i + 1]).append(
+          "<div class='card-block'><h5 class='card-title'>" +
+            response[i].name +
+            "</h5></div>"
+        );
+      }
+    });
+  }
+
+  // grabs 4 random advice
+  function getBadAdvice() {
+    $.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://badadvice.rest/api/count=29",
+        method: "GET",
+      }).then(function(response){
+        // sets random advice to cards
+        console.log(response[0]);
+        for (let i = 0; i < 4; i++){
+          $("#card-back-" + [i + 1]).text(response[i]);
+        }
+      });
+} 
   
- 
+  //   fixed footer styling
+  $(".footer").css("position", "fixed");
+  $(".footer").css("left", "0");
+  $(".footer").css("bottom", "0");
+  $(".footer").css("width", "100%");
+  $(".footer").css("height", "10vh");
+
+  function setPortalGif(URL) {
+    $("#background-gif").css("background-image", "url(" + URL + ")");
+    $("#background-gif").css("background-size", "100% 50%");
+    $("#background-gif").css("background-attachment", "fixed");
+    $("#background-gif").css("height", "50vh");
+  }
+
+  function setBgImg(URL) {
+    $(".jumbotron").css("background-image", "url(" + URL + ")");
+    $(".jumbotron").css("background-size", "100% 50%");
+    // $(".jumbotron").css("background-position", "center center");
+    $(".jumbotron").css("background-attachment", "fixed");
+    // $(".jumbotron").css("background-repeat", "no");
+    $(".jumbotron").css("height", "50vh");
+    // $("content").css("width", "50%");
+    // $("content").css("padding", "40px");
+    // $("content").css("margin", "100px auto");
+  }
+  function clearCards() {
+    for (i = 0; i < 4; i++) {
+      $("#card-front-" + [i + 1]).empty();
+      $("#card-back-" + [i + 1]).empty();
+    }
+  }
+  function clearHomePageText() {
+    $("#jumbotron-text").empty();
+    $("#blockquote-main").empty();
+    $("#blockquote-cite-main").empty();
+    $("#blockquote-cite-main").removeClass("blockquote-footer");
+    $("#game-instructions").empty();
+  }
+});
